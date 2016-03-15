209cb6f245aff6103f74374b615bc269e423cf59
Ariel Elkin <ariel.elkin@project-a.com>
2016-03-15 18:40:17 +0100

    add PaddedView and example code

---
+++ PaddedView/PaddedView.swift
@@ -0,0 +1,71 @@
+//
+//  PaddedView.swift
+//  PaddedView
+//
+//  Created by Ariel Elkin on 15/03/16.
+//  Copyright © 2016 Project A Ventures GmbH. All rights reserved.
+//
+
+import UIKit
+
+/**
+ PaddedView is a `UIView` subclass that sets its `layoutMargins` to
+ `UIEdgeInsetsZero` if its content's height or width is zero.
+
+ `PaddedView` sets its `translatesAutoresizingMaskIntoConstraints`
+ property to `false`.
+
+ */
+class PaddedView: UIView {
+
+    let contentView: UIView
+    var padding: UIEdgeInsets
+
+    init(content: UIView, padding: UIEdgeInsets) {
+        self.contentView = content
+        self.padding = padding
+
+        super.init(frame: CGRectZero)
+
+        self.layoutMargins = padding
+
+        translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(contentView)
+
+        let constraints = [
+            "H:|-[contentView]-|",
+            "V:|-[contentView]-|"
+
+            ].flatMap {
+                NSLayoutConstraint.constraintsWithVisualFormat($0, options: [], metrics: nil, views: ["contentView": self.contentView])
+        }
+        NSLayoutConstraint.activateConstraints(constraints)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if contentView.bounds.size.width == 0 || contentView.bounds.size.height == 0 {
+            self.layoutMargins = UIEdgeInsetsZero
+        }
+        else {
+            self.layoutMargins = padding
+        }
+    }
+
+    //Most padding tends to be at the bottom:
+    convenience init(content: UIView, bottomPadding: CGFloat) {
+        self.init(content: content, padding: UIEdgeInsets(top: 0, left: 0, bottom: bottomPadding, right: 0))
+    }
+
+    convenience init(content: UIView, topPadding: CGFloat) {
+        self.init(content: content, padding: UIEdgeInsets(top: topPadding, left: 0, bottom: 0, right: 0))
+    }
+
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- PaddedView/ViewController.swift
+++ PaddedView/ViewController.swift
@@ -10,16 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let redLabel = UILabel()
+
+    func tapped() {
+        redLabel.text = redLabel.text == nil ? labelText : nil
+    }
+
+    let labelText = "I'm padded. Tap me."
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = UIColor.whiteColor()
+
+        let touchGesture = UITapGestureRecognizer(target: self, action: "tapped")
+        view.addGestureRecognizer(touchGesture)
+
+
+        var viewsDict = [String: UIView]()
+
+        redLabel.text = labelText
+        redLabel.backgroundColor = UIColor.redColor()
+        let redLabelPadded = PaddedView(content: redLabel, bottomPadding: 10)
+        viewsDict["redLabel"] = redLabelPadded
+        view.addSubview(redLabelPadded)
+
+
+        let blueView = UIView()
+        blueView.backgroundColor = UIColor.blueColor()
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        viewsDict["blueView"] = blueView
+        view.addSubview(blueView)
+
+
+
+        let imageView = UIImageView(image: UIImage(named: "juggler"))
+        imageView.contentMode = .Center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        viewsDict["imageView"] = imageView
+        view.addSubview(imageView)
+
+
+        let constraints = [
+            "H:|-[imageView]-|",
+            "H:|-[redLabel]-|",
+            "H:|-[blueView]-|",
+
+            "V:|-30-[imageView][redLabel][blueView(20)]"
+
+            ].flatMap {
+                NSLayoutConstraint.constraintsWithVisualFormat($0, options: [], metrics: nil, views: viewsDict)
+        }
+        
+        NSLayoutConstraint.activateConstraints(constraints)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
